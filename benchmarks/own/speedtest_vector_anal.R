f68421a8d74ee8088719999800c38433e377cd45
Timo Schneider <timos@perlplexity.org>
2013-02-19 14:58:49 +0000

    - updated r script

--- benchmarks/own/speedtest_vector_anal.R
+++ benchmarks/own/speedtest_vector_anal.R
@@ -1,10 +1,16 @@
 library("reshape");
 
-data <- read.table("vector_bench_memcpy.dat", header=TRUE);
+postscript("speedtest_vector_anal.ps");
+
+data <- read.table("kepler_vector.dat.bz2", header=TRUE);
 
 
-adata <- aggregate((mpi_pack / farc_pack) ~ size + blklen + stride + count + pack_count, data=data, FUN=median);
+adata <- aggregate((farc_pack / mpi_pack) ~ size + blklen + stride + count + pack_count, data=data, FUN=median);
 names(adata)[6]<-"performance"
 
 
-plot(x=adata$blklen, y=adata$performance, log="xy");
+plot(x=adata$size, y=adata$performance, log="x", type="p",
+     xlab="Datasize [B]", ylab="Packing time relative to MPI", xaxt="n");
+axis(1, at=c(2e4, 5e4, 1e5, 2e5), labels=c("20K", "50K", "100K", "200K"))
+
+dev.off();
