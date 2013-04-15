337194d50e58d13139f3a170bc70317816d3dd5d
Timo Schneider <timos@perlplexity.org>
2013-04-15 22:07:45 +0200

    use seperate ddts for (interposed) MPI and PMPI
    I forgot to add this in the previous commit

--- pmpi-tests/test_send_vector.c
+++ pmpi-tests/test_send_vector.c
@@ -30,23 +30,30 @@ int main(int argc, char** argv) {
     MPI_Type_vector(2, 3, 5, MPI_INT, &vector_ddt);
     MPI_Type_commit(&vector_ddt);
 
+    MPI_Datatype pmpi_vector_ddt;
+    PMPI_Type_vector(2, 3, 5, MPI_INT, &pmpi_vector_ddt);
+    PMPI_Type_commit(&pmpi_vector_ddt);
+
     if (rank % 2 == 0) {
         MPI_Send(mpi_inbuf, 2, vector_ddt, peer, 0, MPI_COMM_WORLD);
         MPI_Recv(mpi_outbuf, 2, vector_ddt, peer, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);       
 
-        PMPI_Send(pmpi_inbuf, 2, vector_ddt, peer, 0, MPI_COMM_WORLD);
-        PMPI_Recv(pmpi_outbuf, 2, vector_ddt, peer, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);       
+        PMPI_Send(pmpi_inbuf, 2, pmpi_vector_ddt, peer, 0, MPI_COMM_WORLD);
+        PMPI_Recv(pmpi_outbuf, 2, pmpi_vector_ddt, peer, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);       
     }
     else {
         MPI_Recv(mpi_outbuf, 2, vector_ddt, peer, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);       
         MPI_Send(mpi_inbuf, 2, vector_ddt, peer, 0, MPI_COMM_WORLD);
 
-        PMPI_Recv(pmpi_outbuf, 2, vector_ddt, peer, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);       
-        PMPI_Send(pmpi_inbuf, 2, vector_ddt, peer, 0, MPI_COMM_WORLD);
+        PMPI_Recv(pmpi_outbuf, 2, pmpi_vector_ddt, peer, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);       
+        PMPI_Send(pmpi_inbuf, 2, pmpi_vector_ddt, peer, 0, MPI_COMM_WORLD);
     }
 
     int res = compare_buffers(20*sizeof(int), &mpi_inbuf, &pmpi_inbuf, &mpi_outbuf, &pmpi_outbuf);
     free_buffers(&mpi_inbuf, &pmpi_inbuf, &mpi_outbuf, &pmpi_outbuf);
+    MPI_Type_free(&vector_ddt);
+    PMPI_Type_free(&pmpi_vector_ddt);
+
     test_result(res);
 
     MPI_Finalize();
